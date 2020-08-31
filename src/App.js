b9407d26f2694da8bfde77d0d0ea47b71f565b66
Sean <sean.n.cordes@gmail.com>
2020-08-31 00:59:07 -0400

    added keyup event

--- src/App.js
+++ src/App.js
@@ -9,55 +9,90 @@ function Button(props) {
   )
 }
 
+class Keyboard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return(
+      <div>
+        <div>
+          <Button value={'c'} />
+          <Button value={'('} />
+          <Button value={')'} />
+          <Button value={'/'} />
+        </div>
+
+        <div>
+          <Button value={'7'} />
+          <Button value={'8'} />
+          <Button value={'9'} />
+          <Button value={'*'} />
+        </div>
+
+        <div>
+          <Button value={'4'} />
+          <Button value={'5'} />
+          <Button value={'6'} />
+          <Button value={'-'} />
+        </div>
+
+        <div>
+          <Button value={'1'} />
+          <Button value={'2'} />
+          <Button value={'3'} />
+          <Button value={'+'} />
+        </div>
+
+        <div>
+          <Button value={'0'} />
+          <Button value={'.'} />
+          <Button value={'='} />
+        </div>
+      </div>
+    )
+  }
+}
+
+class Display extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render()  {
+    return (
+      <div>
+        <input></input>
+      </div>
+    )
+  }
+}
+
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
     this.state = {
       'input': '',
       'values': [],
     }
   }
 
+  handleKeyPress(e) {
+    console.log(e.key)
+  }
+
+  componentDidMount(){
+    document.addEventListener("keyup", this.handleKeyPress);
+  }
+  componentWillUnmount(){
+    document.removeEventListener("keyup", this.handleKeyPress);
+  }
+
   render() {
     return (
-      <div>
-        <div>
-          <input></input>
-        </div>
-
-        <div>
-          <Button value={'c'} />
-          <Button value={'+/-'} />
-          <Button value={'%'} />
-          <Button value={'/'} />
-        </div>
-
-        <div>
-          <Button value={7} />
-          <Button value={8} />
-          <Button value={9} />
-          <Button value={'x'} />
-        </div>
-
-        <div>
-          <Button value={4} />
-          <Button value={5} />
-          <Button value={6} />
-          <Button value={'-'} />
-        </div>
-
-        <div>
-          <Button value={1} />
-          <Button value={2} />
-          <Button value={3} />
-          <Button value={'+'} />
-        </div>
-
-        <div>
-          <Button value={0} />
-          <Button value={'.'} />
-          <Button value={'='} />
-        </div>
+      <div className='calculator'>
+        <Display />
+        <Keyboard />
       </div>
     )
   }
